afea0cb1e65c8ea4b4d8aeefe6612451be6aba30
Sivan Greenberg <sivan@omniqueue.com>
2012-06-09 18:49:30 +0300

    header-cols

---
+++ header-cols.py
@@ -0,0 +1,41 @@
+ODS_FIELDS = ['Timestamp',
+ 'Link to Qt DevNet profile',
+ 'Qt BugReports profile',
+ 'Full Name',
+ 'Affiliation',
+ 'Reasons for attending',
+ 'Invited by?',
+ 'Visa support?',
+ 'Travel sponsorship?',
+ 'T Shirt Size',
+ 'T Shirt Style',
+ 'Travel cost (estimate)',
+ 'Email address',
+ 'What coverage do you need?',
+ 'Nick',
+ 'Interests',
+ 'Country',
+ 'Arriving From',
+ 'Reason to be sponsored']
+
+
+CSV_FIELDS = ['full-name',
+ 'nickname',
+ 'affiliation',
+ 'interests',
+ 'reasons-for-attending-1',
+ 'invited_by',
+ 'profile',
+ 'copy5_of_full-name',
+ 'travel-sponsorship',
+ 'country_type',
+ 'visa-support',
+ 't-shirt-size',
+ 't-shirt-style',
+ 'email',
+ 'dt']
+
+# this takes ODS field index and converts it
+# to CSV field index.
+CSV_FIELD_DICT = {
+	}
